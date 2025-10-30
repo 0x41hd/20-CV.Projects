2d36a6a15c17f75c584f8230bddc93ac4047dc29
AHD <AHD.CODE@proton.me>
2025-10-30 20:55:01 +0330

    def display_image

--- Pencil-Sketch.py
+++ Pencil-Sketch.py
@@ -15,7 +15,7 @@ def open_file():
         return
     img = cv2.imread(filepath)
     display_image(img, original=True)
-    sketch_image = convert_to_sketch(img)
+    sketch_img = convert_to_sketch(img)
     display_image(sketch_img, original=False)
 
 
@@ -27,3 +27,17 @@ def convert_to_sketch(img):
     sketch_img = cv2.divide(gray_img, inverted_blur_img, scale=256.0)
     return sketch_img
 
+def display_image(img, original):
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) if original else img
+    img_pil = Image.fromarray(img_rgb)
+    img_tk = ImageTk.PhotoImage(image= img_pil)
+
+    #store the PIL in dictionary
+    if original:
+        images["original"] = img_pil
+    else:
+        images["sketch"] = img_pil
+
+    label = original_image_label if original else sketch_image_label
+    label.config(image= img_tk)
+    label.image = img_tk
