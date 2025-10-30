c91bc23aa73fe56636aa4c9faed4231fbfd1194c
AHD <AHD.CODE@proton.me>
2025-10-30 21:31:03 +0330

    app

--- Pencil-Sketch.py
+++ Pencil-Sketch.py
@@ -22,24 +22,25 @@ def open_file():
 def convert_to_sketch(img):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     inverted_img = cv2.bitwise_not(gray_img)
-    blurred_img = cv2.GaussianBlur(inverted_img, (21,21), sigmaX=0, sigmaY=0)
+    blurred_img = cv2.GaussianBlur(inverted_img, (21, 21), sigmaX=0, sigmaY=0)
     inverted_blur_img = cv2.bitwise_not(blurred_img)
     sketch_img = cv2.divide(gray_img, inverted_blur_img, scale=256.0)
     return sketch_img
 
+
 def display_image(img, original):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) if original else img
     img_pil = Image.fromarray(img_rgb)
-    img_tk = ImageTk.PhotoImage(image= img_pil)
+    img_tk = ImageTk.PhotoImage(image=img_pil)
 
-    #store the PIL in dictionary
+    # store the PIL in dictionary
     if original:
         images["original"] = img_pil
     else:
         images["sketch"] = img_pil
 
     label = original_image_label if original else sketch_image_label
-    label.config(image= img_tk)
+    label.config(image=img_tk)
     label.image = img_tk
 
 
@@ -47,11 +48,35 @@ def save_sketch():
     if images["sketch"] is None:
         messagebox.showerror("Error", "No sketch to save.")
         return
-    
-    sketch_filepath = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+
+    sketch_filepath = filedialog.asksaveasfilename(
+        defaultextension=".png", filetypes=[("PNG files", "*.png")])
     if not sketch_filepath:
         return
-    
-    #save PIl image (sketch) to file
+
+    # save PIl image (sketch) to file
     images["sketch"].save(sketch_filepath, "PNG")
     messagebox.showinfo("Saved", "Sketch saved to {}".format(sketch_filepath))
+
+
+app = tk.Tk()
+app.title("Pencil Sketch Converter")
+
+frame = tk.Frame(app)
+frame.pack(pady=10, padx=10)
+
+original_image_label = tk.Label(frame)
+original_image_label.grid(row=0, column=0, padx=5, pady=5)
+sketch_image_label = tk.Label(frame)
+sketch_image_label.grid(row=0, column=1, padx=5, pady=5)
+
+btn_frame = tk.Frame(app)
+btn_frame.pack(pady=10)
+
+open_buttom = tk.Button(btn_frame, text="Open Image", command=open_file)
+open_buttom.grid(row=0, column=0, padx=5)
+
+save_buttom = tk.Button(btn_frame, text="Save Sketch", command=save_sketch)
+save_buttom.grid(row=0, column=1, padx=5)
+
+app.mainloop()
