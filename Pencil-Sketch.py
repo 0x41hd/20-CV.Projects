4ddb9be4abc6487da0e843ba8d876fa852a4757f
AHD <AHD.CODE@proton.me>
2025-10-30 20:22:00 +0330

    def_convert_to_sketch

--- Pencil-Sketch.py
+++ Pencil-Sketch.py
@@ -19,3 +19,11 @@ def open_file():
     display_image(sketch_img, original=False)
 
 
+def convert_to_sketch(img):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    inverted_img = cv2.bitwise_not(gray_img)
+    blurred_img = cv2.GaussianBlur(inverted_img, (21,21), sigmaX=0, sigmaY=0)
+    inverted_blur_img = cv2.bitwise_not(blurred_img)
+    sketch_img = cv2.divide(gray_img, inverted_blur_img, scale=256.0)
+    return sketch_img
+
