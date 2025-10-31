cd31a1dd03f01528ec34bf76cc37cb751b2ae096
AHD <AHD.CODE@proton.me>
2025-10-31 21:19:10 +0330

    open.file-def

---
+++ converter-Gray.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np 
+from tkinter import Tk, filedialog, Button, Label
+from tkinter import N, S, E, W 
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg* .jpeg* .png* .bmp* .tiff")])
+    if file_path:
+        convert_to_graysacale(file_path)
