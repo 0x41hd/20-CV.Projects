bbc8dbfde09a6c1972565b40cd62b8d8a93e07d9
AHD <AHD.CODE@proton.me>
2025-11-05 23:37:03 +0330

    init__def

--- Live_gray.py
+++ Live_gray.py
@@ -1,3 +1,12 @@
 import cv2
 import tkinter as tk
 from tkinter import ttk
+
+
+class LiveGreyscaleFilter:
+    def __init__(Self, root):
+        self.root = root
+        self.root.title("Live Greyscale Filter")
+        self.run_filter = False
+        self.setup_ui()
+        self.cap = cv2.VideoCapture(0)
