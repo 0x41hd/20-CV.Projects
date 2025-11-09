f745d1b8a45e1246a73d296979b2171e89d99c42
AHD <AHD.CODE@proton.me>
2025-11-09 19:05:30 +0330

    seg_def

--- image_seg.py
+++ image_seg.py
@@ -12,3 +12,31 @@ def select_image():
     image = cv2.imread(filepath)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     segment_image(image)
+
+
+def segment_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    _, thresh = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    _, sure_fg = cv2.threshold(
+        dist_transform, 0.7 * dist_transform.max(), 255, 0)
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+    ret, markers = cv2.connectedComponents(sure_fg)
+    markers = markers + 1
+    markers[unknown == 255] = 0
+    markers = cv2.watershed(image, markers)
+    image[markers == -1] = [255, 0, 0]
+
+    display_segmented_image(image)
+
+
+def display_segmented_image(segmented_image):
+    cv2.imshow("Segmented Image", cv2.cvtColor(
+        segmented_image, cv2.COLOR_RGB2BGR))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
