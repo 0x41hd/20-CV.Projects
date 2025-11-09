a8a89702800ef528d3b2c19f1a5d247689d87481
AHD <AHD.CODE@proton.me>
2025-11-09 19:02:56 +0330

    feature_match_def

--- feature-matching.py
+++ feature-matching.py
@@ -17,3 +17,25 @@ def select_image_2():
     if file_path2:
         img2 = cv2.imread(file_path2, cv2.IMREAD_GRAYSCALE)
         label_img2.config(text="Image 2: {}".format(file_path2.split('/')[-1]))
+
+
+def feature_matching():
+    if img1 is None or img2 is None:
+        return
+
+    orb = cv2.ORB_create()
+
+    keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(descriptors1, descriptors2)
+
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    img_matches = cv2.drawMatches(img1, keypoints1, img2, keypoints2,
+                                  matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    cv2.imshow('Feature Matching', img_matches)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
